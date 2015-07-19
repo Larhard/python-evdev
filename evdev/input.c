5abef45c5b883adcafa9a3172905f5c2a4a67136
Larhard <larhard@gmail.com>
2015-07-19 14:38:25 +0200

    add end of device support

--- evdev/input.c
+++ evdev/input.c
@@ -50,8 +50,8 @@ device_read(PyObject *self, PyObject *args)
 
     int n = read(fd, &event, sizeof(event));
 
-    if (n < 0) {
-        if (errno == EAGAIN) {
+    if (n != sizeof(event)) {
+        if (errno == EAGAIN || n == 0) {
             Py_INCREF(Py_None);
             return Py_None;
         }
